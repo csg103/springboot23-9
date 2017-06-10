5d34c2b1fa540bdc34643859bda8f903a130725b
450294533@qq.com <chenshuguang1234>
2017-06-10 18:06:37 +0800

    增加退出 实现ExitCodeGenerator

--- src/main/java/com/csg/Springboot239Application.java
+++ src/main/java/com/csg/Springboot239Application.java
@@ -1,12 +1,22 @@
 package com.csg;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Springboot239Application {
-
+public class Springboot239Application implements CommandLineRunner {
+	@Autowired
+	private HelloWorldService helloWorldService;
 	public static void main(String[] args) {
 		SpringApplication.run(Springboot239Application.class, args);
+	}
+	@Override
+	public void run(String... args) {
+		System.out.println(this.helloWorldService.getMessage());
+		if (args.length > 0 && args[0].equals("exitcode")) {
+			throw new ExitException();
+		}
 	}
 }
