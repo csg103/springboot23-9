6085dbb7c1de6c6bb9e314241940a2fb6501375f
450294533@qq.com <chenshuguang1234>
2017-06-10 17:47:11 +0800

    Initial commit

---
+++ src/main/java/com/csg/Springboot239Application.java
@@ -0,0 +1,12 @@
+package com.csg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot239Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot239Application.class, args);
+	}
+}
---
+++ src/test/java/com/csg/Springboot239ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.csg;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class Springboot239ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
